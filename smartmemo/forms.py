49b9b31a9cb17a92a641777f9e2727414ef52f3e
Takeshi Satake <takeshi.satake.dev@gmail.com>
2026-08-13 09:11:51 +0900

    forms.pyの不要なimportを削除

--- smartmemo/forms.py
+++ smartmemo/forms.py
@@ -6,8 +6,6 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 from .models import Profile
 
 
